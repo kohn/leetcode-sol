73d922ca65e8cea8ce3e5c8914fc4214bde87e25
Kohn Tang <kohntzx@gmail.com>
2016-06-11 01:44:52 +0800

    add an more elegent way

--- MajorityElement.cpp
+++ MajorityElement.cpp
@@ -1,3 +1,22 @@
+// // A More Elegent Way
+// int majorityElement(vector<int>& nums) 
+// {
+//     int count = 0;
+//     int candidate = nums[0];
+//     for(int num : nums)
+//     {
+//         if(count == 0)
+//             candidate = num;
+
+//         if(num == candidate)
+//             ++count;
+//         else
+//             --count;
+//     }
+//     // since "majority element always exist in the array" there is no need to verify candidate
+//     return candidate;
+// }
+
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
