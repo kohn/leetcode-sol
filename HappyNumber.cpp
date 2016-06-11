07a445b4a380433da950be2ef75adc7732605e01
Kohn Tang <kohntzx@gmail.com>
2016-06-11 12:48:12 +0800

    add #202

---
+++ HappyNumber.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    bool isHappy(int n) {
+	unordered_map<int, bool> m;
+	while(1){
+	    int new_number = 0;
+	    while(n!=0){
+		new_number += (n%10)*(n%10);
+		n /= 10;
+	    }
+	    cout << new_number << endl;
+	    if(new_number == 1)
+		return true;
+
+	    if(m.find(new_number) != m.end()){
+		return false;
+	    } else{
+		m[new_number] = true;
+	    }
+	    n = new_number;
+	}
+    }
+};
+
+int main(int argc, char *argv[]) {
+
+    Solution sol;
+    cout << sol.isHappy(19) << endl;
+    cout << sol.isHappy(68) << endl;
+    return 0;
+}
