22697c854b00c8e456ffff32edc88c6542304838
Kohn Tang <kohntzx@gmail.com>
2016-07-23 16:13:38 +0800

    add another elegant method

--- 209.MinimumSizeSubarraySum.cpp
+++ 209.MinimumSizeSubarraySum.cpp
@@ -31,3 +31,24 @@ public:
         return min_size;
     }
 };
+
+
+/**
+ * 网上找的一种更为优雅的实现
+ * 每次外循环中都保证sum<s
+ */
+
+class Solution {
+public:
+    int minSubArrayLen(int s, vector<int>& nums) {
+        int n = nums.size(), start = 0, sum = 0, minlen = INT_MAX;
+        for (int i = 0; i < n; i++) { 
+            sum += nums[i]; 
+            while (sum >= s) {
+                minlen = min(minlen, i - start + 1);
+                sum -= nums[start++];
+            }
+        }
+        return minlen == INT_MAX ? 0 : minlen;
+    }
+};
